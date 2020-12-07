cc76172650e6ea6032160349d1706641020ab40f
MithicSpirit <rpc01234@gmail.com>
2020-12-07 13:08:02 -0500

    Fuzzy matching
    Patches:
     -  Fuzzy highlight
     -  Fuzzy match
     -  DEL: Highlight (conflict with fuzzy highlight)
    
    Also changes color scheme somewhat for better readability.

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Iosevka Mithic Book:size=12"
@@ -10,16 +11,16 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#eeffff", "#292d3e" },
-	[SchemeSel] = { "#eeffff", "#bb80b3" },
-	[SchemeSelHighlight] = { "#ffcb6b", "#bb80b3" },
-	[SchemeNormHighlight] = { "#ffcb6b", "#292d3e" },
+	[SchemeSel] = { "#eeffff", "#4e5579" },
+	[SchemeSelHighlight] = { "#c792ea", "#4e5579" },
+	[SchemeNormHighlight] = { "#c792ea", "#292d3e" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 24;
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
